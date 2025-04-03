44e7126093410aae7eb61351616a3be1e718d1f7
Paul Kim <paul@paulkim.net>
2025-04-03 15:57:25 -0400

    Create plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,21 @@
 class PlansController < ApplicationController
   def show
+    @plan = Current.user.plan || Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    @plan.user = Current.user
+    if @plan.save
+      redirect_to root_path
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:plan_type)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "plans#show"
+  resources :plans, only: [:create]
   resource :session
 
   # resources :passwords, param: :token
