2d5bcbcf6377e8eb52eb3bdeff6d2b329ee57970
Paul Kim <paul@paulkim.net>
2025-04-13 12:33:46 -0400

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :weeks, only: [:update]
   resource :session
 
-  # resources :passwords, param: :token
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
