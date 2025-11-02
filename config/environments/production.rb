882450964e024ef10b32888f2bbb2bbd25d0fe6e
Paul Kim <paul@paulkim.net>
2025-11-01 21:43:23 -0400

    Disable app url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: ENV.fetch("APP_URL") }
+  # config.action_mailer.default_url_options = { host: ENV.fetch("APP_URL") }
   config.action_mailer.delivery_method = :mailpace
   config.action_mailer.mailpace_settings = { api_token: ENV.fetch("MAILPACE_API_TOKEN") }
 
